3ba01a4daee1b30dba440cbd3eab8f1da366b3d2
SamirMan3 <samirshresyha@gmail.com>
2024-04-14 11:09:34 +0545

    Authentication and routes

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('/dashboard', [AuthController::class, 'dashboard']);
+Route::get('/getProfile', [AuthController::class, 'getProfile']);
 Route::get('/getDoctor/{id}', [AuthController::class, 'getDoctor']);
 Route::get('/getProductList', [AuthController::class, 'getProductList']);
 Route::post('/user/register', [AuthController::class, 'userRegister']);
